3d0fb5152acf382d5f719c2529b83e9fe41b96e7
Kevin Marlin <kevin.marlin@gmail.com>
2026-03-24 14:30:31 +0100

    API: add CORS module to secure access data

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
