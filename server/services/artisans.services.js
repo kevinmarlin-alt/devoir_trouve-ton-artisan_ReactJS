4e20f59542c52929de39bd3e5dcea9dbc5480f60
Kevin Marlin <kevin.marlin@gmail.com>
2026-03-27 12:34:35 +0100

    refactor(API): all queries format to support the relationships

--- server/services/artisans.services.js
+++ server/services/artisans.services.js
@@ -1,11 +1,16 @@
-const Artisan = require('../models/Artisan');
+const { Artisan, Speciality } = require('../db/mysql');
 const { Op } = require('sequelize');
 
 
 exports.getTopArtisans = () => {
     return Artisan.findAll({ 
-            where: { top_artisan: true }, 
-            attributes: ['id' ,'rate', 'name', 'speciality', 'city'],
+            where: { top_artisan: true },
+            include: {
+                model: Speciality,
+                required: true,
+                attributes: ['name']
+            },
+            attributes: ['id' ,'rate', 'name', 'city'],
             order: [['rate', 'DESC']]
         })
 }
@@ -14,17 +19,27 @@ exports.getById = (id) => {
     const _id = parseInt(id)
 
     return Artisan.findByPk(_id, {
-            attributes: { exclude: ['category', 'top_artisan'] }
+            attributes: { exclude: ['top_artisan'] },
+            include: {
+                model: Speciality,
+                required: true,
+                attributes: ['name']
+            }
         })
 }
 
 exports.getByName = (name) => {
 
     return Artisan.findAll({
+            attributes: ['id', 'rate', 'name', 'city'],
+            include: {
+                model: Speciality,
+                required: true,
+                attributes: ['name']
+            },
             where: { name: {
                 [Op.like]: `${name}%`
             }}, 
-            attributes: ['id' ,'rate', 'name', 'speciality', 'city'],
             order: [['rate', 'DESC']]
         })
 }
